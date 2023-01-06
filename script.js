e5da2f71b98ae9875e2fddfbcfc4036ec99f149e
shubhangi1329 <117506539+shubhangi1329@users.noreply.github.com>
2023-01-06 21:52:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,7 @@
 let text=document.getElementById('evaluatedText').innerHTML;
 let no=getElementById('letterCount');
 
-if(text==''){
-	no.innerHTML=0;
-
-}else{
+      
 	no.innerHTML=text.length;
-}
+
 
